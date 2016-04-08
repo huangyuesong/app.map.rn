7da4d257d0e4b031ce2c610e52772e185dc8e286
huangyuesong <huangyuesong@meituan.com>
2016-04-08 21:24:14 +0800

    change port to 8081

--- src/webview.js
+++ src/webview.js
@@ -1,23 +1,21 @@
 import React, {
-  Component,
-  StyleSheet,
-  WebView,
+  	Component,
+  	StyleSheet,
+  	WebView,
 } from 'react-native';
 
 export default class WebViewDemo extends Component {
 	render () {
 		return (
 			<WebView
-                source={{uri: 'http://115.28.193.141:8080/'}}
+                source={{uri: 'http://115.28.193.141:8081/'}}
 				automaticallyAdjustContentInsets={false}
 				style={styles.webView}
 				javaScriptEnabled={true}
 				domStorageEnabled={true}
 				decelerationRate="normal"
-				onNavigationStateChange={(navState)=> null}
-				onShouldStartLoadWithRequest={(event)=> {
-					return true;
-				}}
+				onNavigationStateChange={navState=> null}
+				onShouldStartLoadWithRequest={event=> true}
 				startInLoadingState={false}
 				scalesPageToFit={true} />
 		);
